ff8c4c7ad59e472181389e4e4e41122461012043
radishengine <radishengine@gmail.com>
2017-05-09 20:02:14 +0100

    Update denormalize_main.js

--- denormalize_main.js
+++ denormalize_main.js
@@ -9,4 +9,32 @@ function() {
   
   console.log('hello... newman world');
   
+  function onfile(file) {
+    console.log(file);
+  }
+  
+  var dragdrop = document.getElementById('dragdrop');
+  
+  dragdrop.ondragenter = function(e) {
+    if (e.target !== this) return;
+  };
+  
+  dragdrop.ondragover = function(e) {
+    this.classList.add('dropping');
+    e.preventDefault();
+  };
+  
+  dragdrop.ondragleave = function(e) {
+    if (e.target !== this) return;
+    this.classList.remove('dropping');
+  };
+  
+  dragdrop.ondrop = function(e) {
+    e.preventDefault();
+    this.classList.remove('dropping');
+    for (var i = 0; i < e.dataTransfer.files.length; i++) {
+      onfile(e.dataTransfer.files[i]);
+    }
+  };
+  
 });
