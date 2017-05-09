3df88229e40601f6ba3cf41f25dc56bf2e9153bb
radishengine <radishengine@gmail.com>
2017-05-10 00:36:16 +0100

    Update denormalize_main.js

--- denormalize_main.js
+++ denormalize_main.js
@@ -112,6 +112,39 @@ function() {
     },
   };
   
+  function GDVFrameHeader(buffer, byteOffset, byteLength) {
+    this.dv = new DataView(buffer, byteOffset, byteLength);
+  }
+  GDVFrameHeader.prototype = {
+    get signature() {
+      return this.dv.getUint16(0, true);
+    },
+    get hasValidSignature() {
+      return (this.signature === 0x1305);
+    },
+    get dataByteLength() {
+      return this.dv.getUint16(2, true);
+    },
+    get flags() {
+      return this.dv.getUint32(4, true);
+    },
+    get encoding() {
+      return !!(this.flags & 15);
+    },
+    get offset() {
+      return this.flags >>> 8;
+    },
+    get halfResMode() {
+      return !!(this.flags & 32);
+    },
+    get quarterResMode() {
+      return !!(this.flags & 16);
+    },
+    get isShown() {
+      return !!(this.flags & 128);
+    },
+  };
+  
   function GDV(blob) {
     this.blob = blob;
   }
@@ -143,7 +176,124 @@ function() {
         });
       });
       Object.defineProperty(this, 'retrievedPalette', {value:promise, enumerable:true});
-      return promise;      
+      return promise;
+    },
+    get retrievedInterleavedFrames() {
+      var blob = this.blob;
+      var promise = this.retrievedHeader.then(function(header) {
+        if (!header.videoIsPresent) {
+          if (!header.audioIsPresent) return [];
+          var audioOnly = [];
+          var offset = header.frameDataOffset;
+          for (var i = 0; i < header.frameCount; i++) {
+            audioOnly.push(blob.slice(offset, offset + header.audioChunkSize));
+            offset += header.audioChunkSize;
+          }
+          return audioOnly;
+        }
+        var list = [];
+        function nextPart(frameCount, offset) {
+          if (frameCount === 0) return list;
+          if (header.audioIsPresent) {
+            list.push(blob.slice(offset, offset + header.audioChunkSize));
+            offset += header.audioChunkSize;
+          }
+          return blob.slice(offset, offset + 8).then(function(frameHeader) {
+            frameHeader = new GDVFrameHeader(frameHeader.buffer, frameHeader.byteOffset, frameHeader.byteLength);
+            if (!frameHeader.hasValidSignature) {
+              return Promise.reject('invalid frame header');
+            }
+            var frameBlob = blob.slice(offset + 8, offset + 8 + frameHeader.dataByteLength);
+            frameBlob.header = frameHeader;
+            list.push(frameBlob);
+            return nextPart(frameCount - 1, offset + 8 + frameHeader.dataByteLength);
+          });
+        }
+        return nextPart(header.frameCount, header.frameDataOffset);
+      });
+      Object.defineProperty(this, 'retrievedInterleavedFrames', {value:promise, enumerable:true});
+      return promise;
+    },
+    get retrievedVideoFrames() {
+      var self = this;
+      var promise = this.retrievedHeader.then(function(header) {
+        if (!header.videoIsPresent) return [];
+        return self.retrievedInterleavedFrames.then(function(interleaved) {
+          if (!header.audioIsPresent) return interleaved;
+          var videoFrames = new Array(interleaved.length/2);
+          for (var i = 0; i < videoFrames.length; i++) {
+            videoFrames[i] = interleaved[i*2 + 1];
+          }
+          return videoFrames;
+        });
+      });
+      Object.defineProperty(this, 'retrievedVideoFrames', {value:promise, enumerable:true});
+      return promise;
+    },
+    get retrievedAudioFrames() {
+      var self = this;
+      var promise = this.retrievedHeader.then(function(header) {
+        if (!header.audioIsPresent) return [];
+        return self.retrievedInterleavedFrames.then(function(interleaved) {
+          if (!header.videoIsPresent) return interleaved;
+          var audioFrames = new Array(interleaved.length/2);
+          for (var i = 0; i < audioFrames.length; i++) {
+            audioFrames[i] = interleaved[i*2];
+          }
+          return audioFrames;
+        });
+      });
+      Object.defineProperty(this, 'retrievedVideoFrames', {value:promise, enumerable:true});
+      return promise;
+    },
+    getWav: function() {
+      var self = this;
+      var promise = this.retrievedHeader.then(function(header) {
+        if (!header.audioIsPresent) return null;
+        if (!header.videoIsPresent) {
+          var data = [
+            self.blob.slice(
+              header.frameDataOffset,
+              header.frameDataOffset + header.frameCount * header.audioChunkSize)];
+          data.byteLength = data[0].size;
+          return data;
+        }
+        return self.retrievedInterleavedFrames.then(function(interleaved) {
+          var data = new Array(interleaved.length/2);
+          for (var i = 0; i < audioFrames.length; i++) {
+            data[i] = interleaved[i*2];
+          }
+          data.byteLength = interleaved.length * header.audioChunkSize;
+          return data;
+        });
+      });
+      
+      promise = Promise.all([this.retrievedHeader, promise])
+      .then(function(values) {
+        var header = values[0], data = values[1];
+        if (!data) return null;
+        
+        var fmt = new DataView(new AudioBuffer(20));
+        fmt.setUint32(0, 16, true);
+        fmt.setUint16(4, 1, true);
+        fmt.setUint16(6, header.audioChannels, true);
+        fmt.setUint32(8, header.sampleRate, true);
+        fmt.setUint32(12, header.sampleRate * header.audioChannels * header.audioBytesPerSample);
+        fmt.setUint16(16, header.audioChannels * header.audioBytesPerSample);
+        fmt.setUint16(18, header.audioBytesPerSample * 8);
+        
+        var fileSize = new DataView(new AudioBuffer(4));
+        fileSize.setUint32(0, 36 + data.byteLength);
+        
+        var dataSize = new DataView(new AudioBuffer(4));
+        dataSize.setUint32(0, data.byteLength);
+        
+        data.splice(0, 0, 'RIFF', fileSize, 'WAVE', 'fmt ', fmt, 'data', dataSize);
+        
+        return new Blob(data, 'audio/wav');
+      });
+      Object.defineProperty(this, 'retrievedVideoFrames', {value:promise, enumerable:true});
+      return promise;
     },
   };
   
@@ -296,6 +446,26 @@ function() {
           section.stopButton.disabled = true;
         });
         section.stopButton.disabled = true;
+        
+        if (header.audioIsPresent) {
+          section.buttons.push(section.downloadWavButton = document.createElement('BUTTON'));
+          section.downloadWavButton.innerText = 'Download .WAV';
+          section.downloadWavButton.onclick = function() {
+            section.downloadWavButton.disabled = true;
+            gdv.getWav().then(function(blob) {
+              var link = document.createElement('A');
+              link.setAttribute('href', URL.createObjectURL(blob));
+              link.setAttribute('download', (file.name || 'gdv').replace(/\..*/, '') + '.wav');
+              document.body.appendChild(link);
+              link.onclick = function() {
+                link.parentNode.removeChild(link);
+                section.downloadWavButton.disabled = false;
+              };
+              link.click();
+            });
+          };
+        }
+        
       });
     }
     else {
