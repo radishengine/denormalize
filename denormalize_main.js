6446040c759377adf3a58cec7884a01f5e9159b7
radishengine <radishengine@gmail.com>
2017-05-15 23:42:34 +0100

    Update denormalize_main.js

--- denormalize_main.js
+++ denormalize_main.js
@@ -1454,6 +1454,7 @@ function() {
             canvas.height = imageData.height;
             canvas.getContext('2d').putImageData(imageData, 0, 0);
             el.image.appendChild(canvas);
+            el.image.background = 'transparent';
           });
         }
         
