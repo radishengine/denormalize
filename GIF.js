abaadb2779052a16ed7d74618ba17f38a06643da
radishengine <radishengine@gmail.com>
2017-05-16 17:43:00 +0100

    Update GIF.js

--- GIF.js
+++ GIF.js
@@ -2,6 +2,20 @@ define(function() {
 
   'use strict';
   
+  const ONEBYTE_128_255 = (function() {
+    var abuf = new ArrayBuffer(128);
+    var arrays = new Array(128);
+    for (var i = 0; i < 128; i++) {
+      (arrays[i] = new Uint8Array(abuf, i, 1))[0] = 128 + i;
+    }
+    return arrays;
+  })();
+  
+  function oneByte(n) {
+    if (n < 128) return String.fromCharCode(n);
+    return ONEBYTE_128_255[n-128];
+  }
+  
   var GIF = {};
   
   GIF.encode = function(globalPalette, pix8s) {
@@ -9,26 +23,17 @@ define(function() {
     var parts = ['GIF89a', logicalScreenDescriptor];
     logicalScreenDescriptor.setUint16(0, pix8s.width || pix8s[0].width, true);
     logicalScreenDescriptor.setUint16(2, pix8s.height || pix8s[0].height, true);
-    var defaultTransparent = NaN;
+    var defaultTransparent = +pix8s.transparent;
     if (globalPalette) {
       var b = new Uint8Array(globalPalette.buffer, globalPalette.byteOffset, globalPalette.byteLength);
-      var pal = new Uint8Array(Math.pow(2, Math.ceil(Math.log2(globalPalette.length))));
+      var pal = new Uint8Array(3 << Math.ceil(Math.log2(globalPalette.length)));
       for (var i = 0; i < globalPalette.length; i++) {
         pal[i*3] = b[i*4];
         pal[i*3 + 1] = b[i*4 + 1];
         pal[i*3 + 2] = b[i*4 + 2];
       }
       logicalScreenDescriptor.setUint8(4, 0x80 | ((Math.log2(pal.length)-1) << 4));
-      for (var i = 0; i < globalPalette.length; i ++) {
-        if (b[i*4 + 3] === 0) {
-          defaultTransparent = i;
-          break;
-        }
-      }
       parts.push(pal);
-    }
-    function oneByte(n) {
-      return new Uint8Array([n]);
     }
     function pushChunked(extParts) {
       for (var i = 0; i < extParts.length; i++) {
@@ -88,20 +93,7 @@ define(function() {
       var pix8 = pix8s[i];
       var graphicControlExtension = new DataView(new ArrayBuffer(4));
       var localPalette = pix8.palette;
-      var transparent = NaN;
-      var b;
-      if (localPalette) {
-        b = new Uint8Array(localPalette.buffer, localPalette.byteOffset, localPalette.byteLength);
-        for (var j = 0; j < localPalette.length; j++) {
-          if (![j*4+3]) {
-            transparent = j;
-            break;
-          }
-        }
-      }
-      else {
-        transparent = defaultTransparent;
-      }
+      var transparent = ('transparent' in pix8) ? +pix8.transparent : defaultTransparent;
       var packed = isNaN(transparent) ? 0 : 1;
       if (pix8.replace) {
         packed |= (pix8.replace === 'previous') ? (3 << 2) : (2 << 2);
@@ -124,6 +116,7 @@ define(function() {
       if (localPalette) {
         var pal = new Uint8Array(Math.pow(2, Math.ceil(Math.log2(localPalette.length))));
         imageDescriptor.setUint8(9, 0x80 | (Math.log2(pal.length)-1));
+        var b = new Uint8Array(localPalette.buffer, localPalette.byteOffset, localPalette.byteLength);
         for (var i = 0; i < localPalette.length; i++) {
           pal[i*3] = b[i*4];
           pal[i*3 + 1] = b[i*4 + 1];
@@ -132,16 +125,93 @@ define(function() {
         parts.push(pal);
       }
       
-      var minimumCodeSize;
-      var lzw = [];
+      const MAX_CODE_SIZE = 12;
+      var minimumCodeSize = 8;
+      var clearCode = 1 << minimumCodeSize;
+      var endCode = clearCode+1;
+      var nextCode = clearCode+2;
+      var codeSize = minimumCodeSize+1;
+      var validCodeBoundary = 1 << (codeSize+1);
+      var codeTable = Object.create(null);
+      for (var i = 0; i < clearCode; i++) {
+        codeTable[String.fromCharCode(i)] = i;
+      }
+      function getCode(str) {
+        if (str in codeTable) return codeTable[str];
+      }
       
-      throw new Error('NYI');
+      var lzw = new Uint8Array(1 << (Math.ceil(Math.log2(pix8.length))));
+      var pos = 0;
+      
+      function ensure(size) {
+        size += pos;
+        var newSize = lzw.length;
+        if (size <= newSize) return;
+        do { newSize <<= 1; } while (newSize < size);
+        var expanded = new Uint8Array(newSize);
+        expanded.set(lzw);
+        lzw = expanded;
+      }
+      
+      var bufferInt = 0, bufferBits = 0;
+      
+      function write(bits, size) {
+        bufferInt |= bits << bufferBits;
+        if ((bufferBits += size) >= 32) {
+          ensure(4);
+          lzw[pos++] = bufferInt & 0xff;
+          lzw[pos++] = bufferInt >>> 8;
+          lzw[pos++] = bufferInt >>> 16;
+          lzw[pos++] = bufferInt >>> 24;
+          bufferBits -= 32;
+          bufferInt = bits >>> (size - bufferBits);
+        }
+      }
+      
+      function flush() {
+        while (bufferBits > 0) {
+          ensure(1);
+          lzw[pos++] = bufferInt & 0xff;
+          bufferInt >>>= 8;
+          bufferBits -= 8;
+        }
+        pushChunked([lzw.subarray(0, pos)]);
+      }
       
       parts.push(oneByte(minimumCodeSize));
-      pushChunked(lzw);
+      
+      write(clearCode, codeSize);
+      
+      var in_i = 0;
+      var indexBuffer = String.fromCharCode(pix8[in_i++]);
+      while (in_i < pix8.length) {
+        var k = String.fromCharCode(pix8[in_i++]);
+        var buffer_k = indexBuffer+k;
+        if (buffer_k in codeTable) continue;
+        codeTable[buffer_k] = nextCode++;
+        write(codeTable[indexBuffer], codeSize);
+        indexBuffer = k;
+        if (nextCode >= validCodeBoundary) {
+          if (++codeSize >= MAX_CODE_SIZE) {
+            write(clearCode, codeSize);
+            for (k in codeTable) {
+              if (k.length !== 1) delete codeTable[k];
+            }
+            codeSize = minimumCodeSize + 1;
+            validCodeBoundary = 1 << (codeSize + 1);
+          }
+          else {
+            validCodeBoundary <<= 1;
+          }
+        }
+      }
+      write(codeTable[indexBuffer], codeSize);
+      write(endCode, codeSize);
+      
+      flush();
     }
     
-    parts.push(oneByte(0x3B));
+    parts.push(oneByte(0x3B)); // terminator
     
     return new Blob(parts);
   };
