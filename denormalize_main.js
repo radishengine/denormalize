b18d32d189611336adf6e71fa673d9c656d2b6ff
radishengine <radishengine@gmail.com>
2017-05-18 22:34:39 +0100

    Update denormalize_main.js

--- denormalize_main.js
+++ denormalize_main.js
@@ -208,6 +208,7 @@ function(GIF, MGL, GDV, DAS) {
           if (e.which === 27) { // esc
             this.value = '';
             this.blur();
+            this.onchange();
             return;
           }
           else if (this.value === '') this.onchange();
