9f701c613c62dfe81afe22843c4568845056e855
radishengine <radishengine@gmail.com>
2017-05-18 12:10:59 +0100

    Create MGL.js

---
+++ MGL.js
@@ -0,0 +1,150 @@
+define(['blobMethods'], function() {
+
+  'use strict';
+  
+  var MGL = {};
+  
+  MGL.decode = function deMGL(blob) {
+    return blob.readAllBytes().then(function(bytes) {
+      var buf = new Uint8Array(Math.pow(2, Math.ceil(Math.log2(bytes.length + 1))));
+      var in_i = 0, out_i = 0;
+      function ensure(out) {
+        var newSize = buf.length;
+        while ((out_i + out) > newSize) newSize *= 2;
+        if (newSize === buf.length) return;
+        var newBuf = new Uint8Array(newSize);
+        newBuf.set(buf);
+        buf = newBuf;
+      }
+      function allZero(bytes, i, j) {
+        for (; i < j; i++) {
+          if (bytes[i] !== 0) return false;
+        }
+        return true;
+      }
+      decoding: while (in_i < buf.length) {
+        var b = bytes[in_i++];
+        var offset, length, reps;
+        switch (b >>> 4) {
+          case 0x0:
+            if (b === 0) break decoding;
+            // fall through:
+          case 0x1: case 0x2: case 0x3:
+            length = b;
+            if ((in_i + length) > bytes.length) {
+              return Promise.reject('invalid MGL: not enough input');
+            }
+            ensure(length);
+            if (!allZero(bytes, in_i, in_i + length)) {
+              buf.set(bytes.subarray(in_i, in_i + length), out_i);
+            }
+            out_i += length;
+            in_i += length;
+            continue decoding;
+          case 0x4:
+            length = 3 + (b & 0xF);
+            if (out_i < 2) {
+              return Promise.reject('invalid MGL: 2-byte pattern too early');
+            }
+            ensure(length);
+            var state = buf[out_i-1];
+            var inc = state - buf[out_i-2];
+            if (state === 0 && inc === 0) {
+              out_i += length;
+            }
+            else do {
+              buf[out_i] = state += inc;
+              out_i++;
+            } while (--length);
+            continue decoding;
+          case 0x5:
+            length = 2 + (b & 0xF);
+            if (out_i < 4) {
+              return Promise.reject('invalid MGL: 2-word pattern too early');
+            }
+            ensure(length*2);
+            var dv = new DataView(buf.buffer, buf.byteOffset, buf.byteLength);
+            var state = dv.getUint16(out_i-2, true);
+            var inc = state - dv.getUint16(out_i-4, true);
+            if (state === 0 && inc === 0) {
+              out_i += 2 * length;
+            }
+            else do {
+              dv.setUint16(out_i, dv.getUint16(out_i - 2, true) + inc, true);
+              out_i += 2;
+            } while (--length);
+            continue decoding;
+          case 0x6:
+            offset = 1;
+            length = 1;
+            reps = 3 + (b & 0xF);
+            break;
+          case 0x7:
+            offset = 2;
+            length = 2;
+            reps = 2 + (b & 0xF);
+            break;
+          case 0x8: case 0x9: case 0xA: case 0xB:
+            offset = 3 + (b & 0x3F);
+            length = 3;
+            reps = 1;
+            break;
+          case 0xC: case 0xD:
+            offset = 3 + (((b & 3) << 8) | bytes[in_i++]);
+            length = 4 + ((b >>> 2) & 7);
+            reps = 1;
+            break;
+          case 0xE: case 0xF:
+            offset = 3 + (((b & 0x1F) << 8) | bytes[in_i++]);
+            length = 5 + bytes[in_i++];
+            reps = 1;
+            break;
+        }
+        ensure(length * reps);
+        if (offset > out_i) {
+          return Promise.reject('invalid MGL: too far back');
+        }
+        offset = out_i - offset;
+        if ((offset+length) > out_i) {
+          if (allZero(buf, offset, out_i)) {
+            out_i += length * reps;
+            continue;
+          }
+          var copy = buf.subarray(offset, out_i);
+          do {
+            var repLength = length;
+            do {
+              buf.set(copy, out_i);
+              out_i += copy.length;
+              repLength -= copy.length;
+            } while (repLength >= copy.length);
+            if (repLength > 0) {
+              buf.set(copy.subarray(0, repLength), out_i);
+              out_i += repLength;
+            }
+          } while (--reps);
+        }
+        else {
+          if (allZero(buf, offset, offset + length)) {
+            out_i += length * reps;
+            continue decoding;
+          }
+          var copy = buf.subarray(offset, offset + length);
+          do {
+            buf.set(copy, out_i);
+            out_i += length;
+          } while (--reps);
+        }
+      }
+      buf = buf.subarray(0, out_i);
+      var type = '';
+      if (String.fromCharCode.apply(null, buf.slice(0, 6)) === 'DASP\x05\x00') {
+        type = 'application/x-das';
+      }
+      return new Blob([buf], {type:type});
+    });
+  };
+  
+  return MGL;
+
+});
