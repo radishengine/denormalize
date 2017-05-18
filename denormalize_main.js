6bfb37811d1801c8e1559b5ff6928d6a2f6c013e
radishengine <radishengine@gmail.com>
2017-05-18 16:49:34 +0100

    Update denormalize_main.js

--- denormalize_main.js
+++ denormalize_main.js
@@ -116,7 +116,7 @@ function(GIF, MGL, GDV, DAS) {
         section.filter.className = 'filter';
         section.filter.appendChild(section.filter.tagAdder = document.createElement('SELECT'));
         section.filter.tagAdder.appendChild(section.filter.tagAdder.placeholder = document.createElement('OPTION'));
-        section.filter.tagAdder.placeholder.text = 'Tag:';
+        section.filter.tagAdder.placeholder.text = '+tag:(...)';
         section.filter.tagAdder.placeholder.value = '';
         section.filter.tagAdder.placeholder.disabled = true;
         section.filter.tagAdder.placeholder.selected = true;
